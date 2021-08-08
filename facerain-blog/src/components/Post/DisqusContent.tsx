573f9fb8386d74179a1267570da3202b2d4f8b8f
FacerAin <ywsong.dev@kakao.com>
2021-08-08 12:16:21 +0000

    Fix Disqus Layout

--- facerain-blog/src/components/Post/DisqusContent.tsx
+++ facerain-blog/src/components/Post/DisqusContent.tsx
@@ -1,23 +1,40 @@
 import React, { FunctionComponent } from 'react';
-import { Disqus, CommentCount } from 'gatsby-plugin-disqus'
-interface DisqusContentProps{
+import { Disqus, CommentCount } from 'gatsby-plugin-disqus';
+import styled from '@emotion/styled';
+
+interface DisqusContentProps {
 	title: string;
 	siteUrl: string;
 	pathname: string;
 	id: string;
-	
 }
-const DisqusContent:FunctionComponent<DisqusContentProps> = function({siteUrl, pathname, id, title}) {
+
+const DisqusContentWrapper = styled.div`
+margin: 0 200px;
+@media (max-width: 768px) {
+margin: 0 50px;
+
+}
+`;
+
+const DisqusContent: FunctionComponent<DisqusContentProps> = function ({
+	siteUrl,
+	pathname,
+	id,
+	title,
+}) {
 	let disqusConfig = {
-	url: `${pathname}`,
-    identifier: id,
-    title: title,
-	}
-	return(
-	<>
-		<Disqus config={disqusConfig}/>
-	</>
-	)
-}
+		url: `${pathname}`,
+		identifier: id,
+		title: title,
+	};
+	return (
+		<>
+			<DisqusContentWrapper>
+				<Disqus config={disqusConfig} />
+			</DisqusContentWrapper>
+		</>
+	);
+};
 
 export default DisqusContent;
